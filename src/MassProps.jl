6a9006ccdc169b84a343595cac5801ed43e26fff
James Steven Jenkins <sjenkins@studioj.us>
2026-02-16 15:11:14 -0500

    get_mass_props() passes tests

--- src/MassProps.jl
+++ src/MassProps.jl
@@ -1,5 +1,25 @@
 module MassProps
 
-# Write your package code here.
+    using RollupTree
+
+    get_mass_props(table, id) = begin
+        row = RollupTree.df_get_row_by_id(table, id)
+        poi_factor = row.POIconv == "-" ? 1.0 : -1.0
+ 
+        (
+            mass = row.mass,
+
+            center_mass = [row.Cx, row.Cy, row.Cz],
+
+            poi_conv = row.POIconv,
+
+            inertia = [             row.Ixx, poi_factor * row.Ixy, poi_factor * row.Ixz,
+                       poi_factor * row.Ixy,        row.Iyy, poi_factor * row.Iyz,
+                       poi_factor * row.Ixz, poi_factor * row.Iyz,              row.Izz
+            ],
+
+            point = row.Ipoint
+        )
+    end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,71 @@
-using MassProps
-using Test
+using TestItems
 
-@testset "MassProps.jl" begin
-    # Write your tests here.
+@testsnippet Setup begin
+    using Artifacts
+    using DataFrames
+    using CSV
+    using MassProps
+
+    data_dir = artifact"mass_props_data"
+
+    read_data(filename) = CSV.read(joinpath(data_dir, filename), DataFrame; missingstring = "NA",delim = "\t")
+
+    mp_table = read_data("mp_table.tsv")
+    mp_edges = CSV.read(joinpath(data_dir, "mp_edges.tsv"), DataFrame; delim = "\t")
+    
+    sawe_table = CSV.read(joinpath(data_dir, "sawe_table.tsv"), DataFrame; delim = "\t")
+    sawe_edges = CSV.read(joinpath(data_dir, "sawe_edges.tsv"), DataFrame; delim = "\t")
+    
+    test_table = CSV.read(joinpath(data_dir, "test_table.tsv"), DataFrame; delim = "\t")
+    test_edges = CSV.read(joinpath(data_dir, "test_edges.tsv"), DataFrame; delim = "\t")
+end
+
+@testitem "get_mass_props() for positive convention" setup = [Setup] begin
+
+    result = MassProps.get_mass_props(mp_table, "C.1.2.2.3.1.2.3")
+    row = mp_table[findfirst(mp_table.id .== "C.1.2.2.3.1.2.3"), :]
+
+    @test result isa NamedTuple
+
+    @test result.mass == row.mass
+
+    @test result.center_mass isa Vector{Float64}
+    @test result.center_mass == [row.Cx, row.Cy, row.Cz]
+
+    @test result.poi_conv == row.POIconv
+    @test result.poi_conv == "+"
+
+    @test result.inertia isa Vector{Float64}
+    @test result.inertia == [ row.Ixx, -row.Ixy, -row.Ixz,
+                             -row.Ixy,  row.Iyy, -row.Iyz,
+                             -row.Ixz, -row.Iyz,  row.Izz
+                            ]
+
+    
+                                
+end
+    
+@testitem "get_mass_props() for negative convention" setup = [Setup] begin
+
+    result = MassProps.get_mass_props(mp_table, "C.1.2.2.3.2.1.1")
+    row = mp_table[findfirst(mp_table.id .== "C.1.2.2.3.2.1.1"), :]
+
+    @test result isa NamedTuple
+
+    @test result.mass == row.mass
+
+    @test result.center_mass isa Vector{Float64}
+    @test result.center_mass == [row.Cx, row.Cy, row.Cz]
+
+    @test result.poi_conv == row.POIconv
+    @test result.poi_conv == "-"
+
+    @test result.inertia isa Vector{Float64}
+    @test result.inertia == [row.Ixx, row.Ixy, row.Ixz,
+                             row.Ixy, row.Iyy, row.Iyz,
+                             row.Ixz, row.Iyz, row.Izz
+                            ]
+
+    
+                                
 end
